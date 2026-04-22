62fafa2bbe5a16b995bba931ef8fa540b2681dcd
Syed Nasharudin <s.nasharudin@gmail.com>
2026-04-22 09:04:43 +0800

    feat: initial laravel package setup

---
+++ src/SyednPackageProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Syedn\Helper;
+
+use Illuminate\Support\ServiceProvider;
+
+class SyednPackageServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Publish config, load routes, or views here
+    }
+
+    public function register()
+    {
+        // Bind classes to the container
+    }
+}

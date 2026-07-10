f0a75042a481bbd87929425db9b4c0814ea60e24
Syed Nasharudin <s.nasharudin@gmail.com>
2026-07-10 11:17:12 +0800

    - added no back for security reason

---
+++ src/Middlewares/PreventBackHistory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Syedn\Helper\Middlewares;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PreventBackHistory
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        // Instruct the browser to wipe cache parameters for this response
+        return $response->header('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate')
+                        ->header('Pragma', 'no-cache')
+                        ->header('Expires', 'Sun, 02 Jan 1990 00:00:00 GMT');
+    }
+}
